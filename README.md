8474d02eebd2eeccde5623986fd99bc825346cfe
Googler <knowbeeinc@gmail.com>
2020-04-16 17:20:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# klean
+
+    You would be surprised if you knew that images and videos you see online are never deleted from your computer,and they say sensitive data can lead to one's downfall🤷‍♀️....well klean's coming to the rescue
+
+## Installation
+
+```
+yarn global add klean
+```
+
+```
+npm install -g klean
+```
+
+## Usage
+
+```
+  klean
+```
+
+## Contribution
+
+- Please before making a PR, read first this [Contributing Guideline](./CONTRIBUTING.md)
+
+## License
+
+MIT
+
+## Author
+
+Igwaneza Bruce
+
+<knowbeeinc@gmail.com>
---
+++ index.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+const os = require("os");
+
+const slack = os.homedir() + "\\AppData\\Roaming\\Slack\\Cache";
+const chrome =
+  os.homedir() + "\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Cache";
+
+const cachesDirs = [
+  { name: "slack", path: slack },
+  { name: "chrome", path: chrome },
+];
+
+// get cached media size
+const cacheChecker = (dir) => {
+  const files = fs.readdirSync(dir);
+  const sum = files.reduce((sum, file) => {
+    sum += fs.statSync(dir + "\\" + file).size;
+    return sum;
+  }, 0);
+  return sum;
+};
+
+const getTotalSize = () => {
+  let total = 0;
+  cachesDirs.forEach((cacheDir) => {
+    total += cacheChecker(cacheDir.path);
+  });
+  if (total.toString().length > 6)
+    return Math.floor(total / (1024 * 1024)).toString() + " Mbs";
+  if (total.toString().length <= 6)
+    return Math.floor(total / 1024).toString() + " Kbs";
+};
+// delete cached media from the system
+const deleteCache = () => {
+  try {
+    cachesDirs.forEach((cacheDir) => {
+      const files = fs.readdirSync(cacheDir.path);
+      files.forEach((file) => {
+        fs.unlinkSync(cacheDir.path + "\\" + file);
+      });
+      console.log("cleaning ", cacheDir.name);
+    });
+  } catch (error) {
+    console.log(`You should close open applications to clean everything `);
+    process.exit(0);
+  }
+};
+console.log("Found", getTotalSize(), " of cached media");
+console.log("starting clean up process..");
+deleteCache();
+console.log("Done");
